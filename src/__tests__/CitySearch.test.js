247eaa360fe4fdc80b302dbc01125990a70d8568
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-08 16:23:31 +0000

    test: hide suggestions by default, get list

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,6 +2,7 @@
 /* eslint-disable testing-library/render-result-naming-convention */
 import { render } from "@testing-library/react";
 import CitySearch from "../components/CitySearch";
+import { userEvent } from "@testing-library/user-event";
 
 // describe new component scope
 describe("<CitySearch /> component", () => {
@@ -12,4 +13,23 @@ describe("<CitySearch /> component", () => {
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass("city");
     });
+
+    test("suggestions list is hidden by default", () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const suggestionList = CitySearchComponent.queryByRole("list");
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    test("renders a list of suggestions when city textbox gains focus", async () => {
+        const CitySearchComponent = render(<CitySearch />);
+        // set user
+        const user = userEvent.setup();
+        const cityTextBox = CitySearch.queryByRole("textbox");
+        // await user interaction
+        await user.click(cityTextBox);
+        // define suggestionList once input is done
+        const suggestionList = CitySearchComponent.queryByRole("list");
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass("suggestions");
+    })
 });
