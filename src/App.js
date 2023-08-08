4a270421b2354d6e4aed172021d064a04384951a
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-08 02:09:21 +0000

    feat: add EventList to pass test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import EventList from './components/EventList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      < EventList />
     </div>
   );
 }
