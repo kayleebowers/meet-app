837e02ebbea232cf77044317648b7ee78a8f37ee
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-08 15:32:44 +0000

    refactor: define test component in beforeEach

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,23 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/no-node-access */
 import { render } from "@testing-library/react";
 import App from "../App";
 
 // define scope called <App /> component
 describe("< App /> component", () => {
+    let AppDOM;
+    beforeEach(() => {
+        // get App's first child
+        AppDOM = render(< App />).container.firstChild;
+    })
+
     test("renders list of events", () => {
-        const AppDOM = render(< App />).container.firstChild;
         expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
     });
+
     test("render city search", () => {
-        const AppDOM = render(< App />).container.firstChild;
         expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
     });
+
 });
 
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,16 +1,25 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
+
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 describe("<EventList /> component", () => {
+    let EventListComponent;
+    beforeEach(() => {
+        // get outcome of render
+        EventListComponent = render(<EventList />);
+    });
+
     test("has an element with 'list' role", () => {
-        const EventListComponent = render(<EventList />);
         expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
+
     test("renders correct number of events", () => {
-        const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+        // rerender to include events prop
+       EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
         expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
     })
 });
