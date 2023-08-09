f6483382bb51cb7162283773dfd90baa7106caa1
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 19:39:18 +0000

    refactor: add numberTextbox to beforeEach

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,22 +1,32 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
 
 import { render } from "@testing-library/react";
 import NumberOfEvents from "../components/NumberOfEvents";
+import userEvent from "@testing-library/user-event";
 
 describe("<NumberOfEvents /> component", () => {
-    let NumberOfEventsComponent;
+    let NumberOfEventsComponent,
+        numberTextbox;
+
     beforeEach(() => {
         NumberOfEventsComponent = render(<NumberOfEvents />);
+        numberTextbox = NumberOfEventsComponent.getByRole("textbox");
     })
 
+    // test for existing element and default value
     test("render NumberOfEvents textbox", () => {
-        const numberTextbox = NumberOfEventsComponent.getByRole("textbox");
         expect(numberTextbox).toBeInTheDocument();
     });
 
     test("textbox has default value of 32", () => {
-        const numberTextbox = NumberOfEventsComponent.getByRole("textbox");
         expect(numberTextbox).toHaveValue("32");
     });
+
+    // update value on user change
+    test("change textbox value when user types new number", async () => {
+        const user = userEvent.setup();
+        
+    })
 })
