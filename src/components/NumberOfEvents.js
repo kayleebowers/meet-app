91c4e9cda87244dba9cc8f6ed7d81ebc53384476
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 20:27:59 +0000

    feat: update input value in NumberOfEvents

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const NumberOfEvents = () => {
-    return (
-        <input id="event-number" type="text" value="32" ></input>
-    )
-}
+  const [isValue, setValue] = useState("32");
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+  return (
+    <form>
+      <label>Enter a number between 1 and 32</label>
+      <input
+        id="event-number"
+        type="text"
+        value={isValue}
+        onChange={handleChange}
+      ></input>
+    </form>
+  );
+};
 
 export default NumberOfEvents;
