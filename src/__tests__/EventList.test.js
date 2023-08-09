774af4b8209d5ecf943c6385b399f4e14f45bfed
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 03:18:07 +0000

    test: include mock data instead of basic objects

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -5,6 +5,7 @@
 
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
+import { getEvents } from '../api';
 
 describe("<EventList /> component", () => {
     let EventListComponent;
@@ -17,9 +18,10 @@ describe("<EventList /> component", () => {
         expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
 
-    test("renders correct number of events", () => {
+    test("renders correct number of events", async () => {
+        const allEvents = await getEvents();
         // rerender to include events prop
-       EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+       EventListComponent.rerender(<EventList events={allEvents} />);
+       expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
     })
 });
