aabf65ca1b33585ad5229ad1b63ad5af68870d91
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 02:37:40 +0000

    test: handle input change in CitySearch

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,13 +1,37 @@
 import { useState } from "react";
 
-const CitySearch = () => {
-    const [showSuggestions, setShowSuggestions] = useState(false);
-    return (
-        <div id="city-search">
-            <input type="text" className="city" placeholder="Search for a city" onFocus={() => setShowSuggestions(true)} />
-            { showSuggestions ? <ul className="suggestions"></ul> : null}
-        </div>
-    )
-}
+const CitySearch = ({ allLocations }) => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  //handle input change
+  const handleInputChange = ((event) => {
+    //get current input value
+    const value = event.target.value;
+    
+    //filter all locations to match input
+    const filteredLocations = allLocations ? allLocations.filter((location) => {
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    }) : [];
+
+    setQuery(value);
+    setSuggestions(filteredLocations);
+  })
+
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        value={query}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChange}
+      />
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
+    </div>
+  );
+};
 
 export default CitySearch;
