a0f87e19daaf3c217525ec0db24fcac2eb1c0712
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 17:44:10 +0000

    feat: add code to pass render Event title

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -3,13 +3,15 @@
 
 import { render } from "@testing-library/react";
 import Event from "../components/Event";
-import { getEvents } from "../api";
+// import { getEvents } from "../api";
+import mockData from "../mock-data";
 
 describe("<Event /> component", () => {
     test("render Event title", async () => {
-        const events = await getEvents;
-        console.log(events);
-        const eventComponent = render(<Event events={events[0]}/>);
-        expect(eventComponent.getByText(events[0].summary)).toBeInTheDocument();
-    })
+        const testData = mockData[0];
+        const eventComponent = render(<Event event={testData} />);
+        expect(eventComponent.getByText(testData.summary)).toBeInTheDocument();
+    });
+
+    
 })
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,13 @@
-import { getEvents } from "../api";
+// import { getEvents } from "../api";
 
-const Event = () => {
-    return (
-        <li></li>
-    )
-}
+const Event = ({ event }) => {
+  return (
+    <li>
+        <h3>{event.summary}</h3>
+        <p>{event.description}</p>
+        <p>{event.location}</p>
+    </li>
+  )
+};
 
 export default Event;
