460bf8b224c924dbf8a5bf4a91cf16d450f100f6
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 00:23:03 +0000

    test: update list of suggestions when typed input

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -29,11 +29,37 @@ describe("<CitySearch /> component", () => {
     // set user
     const user = userEvent.setup();
     const cityTextBox = CitySearchComponent.queryByRole("textbox");
+
     // await user interaction
     await user.click(cityTextBox);
+    
     // define suggestionList once input is done
     const suggestionList = CitySearchComponent.queryByRole("list");
     expect(suggestionList).toBeInTheDocument();
     expect(suggestionList).toHaveClass("suggestions");
   });
+
+  test("updates list of suggestions correctly when user types in city textbox", async () => {
+    const user = userEvent.setup();
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+    //user types berlin into text box
+    const cityTextBox = CitySearchComponent.queryByRole("textbox");
+    await user.type(cityTextBox, "Berlin");
+
+    // filter allLocations to those matching "Berlin"
+    const suggestions = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+    }): [];
+
+    //get all li in suggestions list
+    const suggestionListItems = CitySearchComponent.queryByRole("listitem");
+    expect(suggestionListItems).toHaveLength(suggestionListItems.length + 1);
+    for (let i = 0; i < suggestions.length; i++) {
+      expect(suggestionListItems[i].textContent).toBe(suggestions);
+    }
+  });
+
 });
