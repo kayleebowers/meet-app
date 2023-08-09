8f33ba13a69cb3c56c928b3f4670982204b06459
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 23:57:20 +0000

    feat: fetch data from getEvents in App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
-import './App.css';
-import mockData from './mock-data';
 import NumberOfEvents from './components/NumberOfEvents';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getEvents } from './api';
+import './App.css';
 
 function App() {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE ] = useState(32);
 
+  // fetch event data
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      < NumberOfEvents />
       < CitySearch />
+      < NumberOfEvents />
       < EventList events={events}/>
     </div>
   );
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -32,7 +32,7 @@ describe("<EventList /> integration", () => {
     test("renders a list of 32 events when the app is mounted and rendered", async () => {
         const AppComponent = render(<App />);
         const AppDOM = AppComponent.container.firstChild;
-        const EventListDOM = AppDOM.querySelector("event-list");
+        const EventListDOM = AppDOM.querySelector("#event-list");
         await waitFor(() => {
             const EventListItems = within(EventListDOM).queryAllByRole("listitem");
             expect(EventListItems.length).toBe(32);
