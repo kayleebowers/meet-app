87a85e443dc1b8653734a214f52d56b755a0e74c
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 21:27:23 +0000

    feat: add events state to App and pass prop

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import CitySearch from './components/CitySearch';
 import './App.css';
 import mockData from './mock-data';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useState } from 'react';
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE ] = useState(32);
+
   return (
     <div className="App">
       < NumberOfEvents />
       < CitySearch />
-      < EventList events={mockData}/>
+      < EventList events={events}/>
     </div>
   );
 }
