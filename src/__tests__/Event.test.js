4d2d33001406c52c16ba603f15c33cf0e9bb5d37
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-09 17:50:44 +0000

    test: render start time and location

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
 
@@ -7,11 +8,23 @@ import Event from "../components/Event";
 import mockData from "../mock-data";
 
 describe("<Event /> component", () => {
-    test("render Event title", async () => {
-        const testData = mockData[0];
-        const eventComponent = render(<Event event={testData} />);
+    let testData,
+        eventComponent;
+
+    beforeEach(() => {
+        testData = mockData[0];
+        eventComponent = render(<Event event={testData} />);
+    })
+
+    test("render Event title", () => {
         expect(eventComponent.getByText(testData.summary)).toBeInTheDocument();
     });
 
-    
+    test("render Event start time", () => {
+        expect(eventComponent.getByText(testData.created)).toBeInTheDocument();
+    });
+
+    test("render Event location", () => {
+        expect(eventComponent.getByText(testData.location)).toBeInTheDocument();
+    })
 })
--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,7 @@ const Event = ({ event }) => {
   return (
     <li>
         <h3>{event.summary}</h3>
-        <p>{event.description}</p>
+        <p>{event.created}</p>
         <p>{event.location}</p>
     </li>
   )
