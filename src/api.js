71180ff28d07f80905ac9b205f61bea0166323e0
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-10 02:59:47 +0000

    feat: get access token when not in localStorage

--- src/api.js
+++ src/api.js
@@ -11,3 +11,27 @@ export const extractLocations = ((events) => {
 export const getEvents = async () => {
     return mockData;
 }
+
+//get access token
+export const getAccessToken = async () => {
+    //check if one exists in localStorage
+    const accessToken = localStorage.getItem("access_token");
+
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+            const response = await fetch (
+                // "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+            );
+            const result = await response.json();
+            const { authURL } = result;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
+};
