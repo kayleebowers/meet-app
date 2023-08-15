352008872f9f1e0637034de65249b48f68df7146
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-15 16:35:25 +0000

    test: add hide event details by default

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,19 +1,32 @@
+/* eslint-disable testing-library/no-node-access */
 import { loadFeature, defineFeature } from "jest-cucumber";
+import { render, waitFor, within } from "@testing-library/react";
+import App from "../App";
 
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 
 defineFeature(feature, test => {
     test('Hide event details by default.', ({ given, when, then }) => {
+        let AppComponent;
+        // simulate opening app
         given('the main page is open', () => {
-
+            AppComponent = render(<App />);
         });
-
+       
         when('the user is viewing all the events', () => {
-
+            // nothing changes
         });
 
-        then('the event elements will be collapsed by default.', () => {
+        then('the event elements will be collapsed by default.', async () => {
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector("#event-list");
 
+            // await event list and check event for null event details element
+            await waitFor(() => {
+                const eventListItems = within(EventListDOM).queryAllByRole("listitem");
+                const eventDetails = eventListItems[0].querySelector(".event-details");
+                expect(eventDetails).toBeNull();
+            })
         });
     });
 
