b02cbd82f47586d3ba649fcdddc6054a5785f96f
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-15 02:20:40 +0000

    refactor: add beforeall and afterall to end tests

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,34 +1,35 @@
 import puppeteer from "puppeteer"
 
 describe("show/hide an event details", () => {
-    test("An event element is collapsed by default", async () => {
-        //launch browser
-        const browser = await puppeteer.launch();
+    let browser;
+    let page;
 
-        // create new page and navigate to locally hosted app
-        const page = await browser.newPage();
+    beforeAll(async () => {
+        //launch browser and load new page
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+
+        //navigate to locally hosted app 
         await page.goto("http://localhost:3000/");
 
         // select event element once it appears
         await page.waitForSelector(".event");
+    });
 
-        const eventDetails = await page.$(".event-details");
-        expect(eventDetails).toBeNull();
+    afterAll(async () => {
         browser.close();
     });
 
-    test("User can expand an event to see its details", async () => {
-        // simulate opening browser and application
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto("http://localhost:3000/");
+    test("An event element is collapsed by default", async () => {
+        const eventDetails = await page.$(".event-details");
+        expect(eventDetails).toBeNull();
+    });
 
+    test("User can expand an event to see its details", async () => {
         // simulate user clicking on the details button
-        await page.waitForSelector(".event");
         await page.click(".details-btn");
 
-        const eventDetails = await page.$(".details-btn");
+        const eventDetails = await page.$(".event-details");
         expect(eventDetails).toBeDefined();
-        browser.close();
     });
 })
