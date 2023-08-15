abf79ae763137e1aa53fb0487bdd0733fd7ee639
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-15 02:07:42 +0000

    test: end to end collapsible element

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -15,5 +15,20 @@ describe("show/hide an event details", () => {
         const eventDetails = await page.$(".event-details");
         expect(eventDetails).toBeNull();
         browser.close();
-    })
+    });
+
+    test("User can expand an event to see its details", async () => {
+        // simulate opening browser and application
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("http://localhost:3000/");
+
+        // simulate user clicking on the details button
+        await page.waitForSelector(".event");
+        await page.click(".details-btn");
+
+        const eventDetails = await page.$(".details-btn");
+        expect(eventDetails).toBeDefined();
+        browser.close();
+    });
 })
