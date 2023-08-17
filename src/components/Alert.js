b61e3e57f328aa7f1ec980a53fd1c6c64bd67a6f
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-17 18:12:34 +0000

    style: match InfoAlert to app colors

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -35,8 +35,8 @@ class Alert extends Component {
 class InfoAlert extends Alert {
     constructor(props) {
         super(props);
-        this.color = "rgb(0, 0, 255)";
-        this.bgColor = "rgb(220, 220, 255)";
+        this.color = "black";
+        this.bgColor = "#bcdcdb";
     };
 };
 
