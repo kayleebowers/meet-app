55d2b017469daa783a4d6696183d58bd7f3082e5
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-17 20:12:06 +0000

    feat: define error message for number of events

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const [isValue, setValue] = useState("32");
 
   const handleChange = (event) => {
     const value = event.target.value;
     setValue(value);
     setCurrentNOE(value);
+
+    // define error message if value is not a number
+    if (value <= 0) {
+      setErrorAlert("Your search number must be greater than 0");
+      setCurrentNOE("32");
+    } else if (isNaN(value)) {
+      setErrorAlert("Your search must be a positive number");
+      setCurrentNOE("32");
+    } else {
+      setErrorAlert("");
+    }
   };
 
   return (
