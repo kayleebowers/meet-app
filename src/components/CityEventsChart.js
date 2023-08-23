28a16e358a55617616b8811a81505cf931f58a67
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-23 18:33:44 +0000

    feat: add function to get # of events per city

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react";
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-const CityEventsChart = () => {
-    
+const CityEventsChart = ({events, allLocations}) => {
+    // get number of events in each city
+    const getData = () => {
+        const data = allLocations.map((location) => {
+            const count = events.filter((event) => event.location === location).length;
+            const city = location.split(", ")[0];
+            return {count, city};
+        })
+        return data;
+    }
 }
 
 export default CityEventsChart;
