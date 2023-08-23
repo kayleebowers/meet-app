e19c208d7a3d737009a0db80dc7f440f582f9453
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-23 20:45:38 +0000

    feat: add responsive PieChart component

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,27 +1,63 @@
 import { useState, useEffect } from "react";
-import { PieChart, Pie, Cell } from "recharts";
+import { ResponsiveContainer, PieChart, Pie } from "recharts";
 
-const EventGenresChart = ({events}) => {
-    const [data, setData] = useState([]);
-    const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
 
-    // get data only on event change
-    useEffect(() => {
-        setData(getData());
+  // get data only on event change
+  useEffect(() => {
+    setData(getData());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, JSON.stringify(events));
+  }, JSON.stringify(events));
 
-    // calculate number of genre events from data
-    const getData = () => {
-        const data = genres.map((genre) => {
-            const genreEventsNumber = events.filter((event) => event.summary.includes(genre)).length;
-            return {
-                name: genre,
-                value: genreEventsNumber
-            };
-        });
-        return data;
-    }
-}
+  // calculate number of genre events from data
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const genreEventsNumber = events.filter((event) =>
+        event.summary.includes(genre)
+      ).length;
+      return {
+        name: genre,
+        value: genreEventsNumber,
+      };
+    });
+    return data;
+  };
+
+  // render customized pie segment labels
+  const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={130}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default EventGenresChart;
