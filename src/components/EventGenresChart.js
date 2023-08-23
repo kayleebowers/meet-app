22403214f16db98d7afa90ae12e5e9a8d2e92e85
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-23 20:41:42 +0000

    feat: compile event data for EventGenresChart

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -2,8 +2,26 @@ import { useState, useEffect } from "react";
 import { PieChart, Pie, Cell } from "recharts";
 
 const EventGenresChart = ({events}) => {
-    const [data, setData] = useState[];
+    const [data, setData] = useState([]);
     const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+
+    // get data only on event change
+    useEffect(() => {
+        setData(getData());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, JSON.stringify(events));
+
+    // calculate number of genre events from data
+    const getData = () => {
+        const data = genres.map((genre) => {
+            const genreEventsNumber = events.filter((event) => event.summary.includes(genre)).length;
+            return {
+                name: genre,
+                value: genreEventsNumber
+            };
+        });
+        return data;
+    }
 }
 
 export default EventGenresChart;
