9e255d698ceb90a33495ae3772d7c1f350173629
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-24 14:47:15 +0000

    style: match CityEventsChart style to app colors

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -33,8 +33,8 @@ const CityEventsChart = ({events, allLocations}) => {
           <CartesianGrid />
           <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{dx: 20, dy: 40, fontSize: 14}} />
           <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false}/>
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="A school" data={data} fill="#8884d8" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} contentStyle={{color: "black", backgroundColor: "#7C9D96", borderRadius: "10px", display: "flex", justifyContent: "center", border: "none", padding: "3px"}}/>
+          <Scatter name="A school" data={data} fill="#7C9D96" />
         </ScatterChart>
       </ResponsiveContainer>
     )
